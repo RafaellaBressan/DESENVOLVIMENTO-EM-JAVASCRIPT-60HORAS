16bd7498f5d98d8fb607f4b32945747f064d867e
RafaellaBressan <rafaellabressandacosta14@gmail.com>
2026-02-18 21:54:39 -0300

    Add files via upload

---
+++ aula_7ativid2/script.js
@@ -0,0 +1,22 @@
+let numero = prompt ('Digite um número')
+
+if (numero >0) {
+console.log('O número é positivo')
+}
+
+else {
+console.log('O número é negativo ou zero')
+}
+
+let idade = prompt ('Digite sua idade')
+
+if (idade >=18) {
+console.log('Você pode votar')
+}
+
+else {
+console.log('Você não pode votar')
+}
+
+let numeroimpar = prompt (8)
+
